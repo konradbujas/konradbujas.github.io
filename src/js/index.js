29fcf9a5a3455f4a7295f256faf8521391dd5f1c
Konrad Bujas <kondziobu@gmail.com>
2022-06-02 13:44:15 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,24 @@ import "../scss/main.scss";
 
 console.log("Hello, I'm Konrad. Nice to meet you 🚀");
 
+// dark-mode--js
+
+let isDark = false ;
+const switchModes = document.querySelector('.mode--js');
+switchModes.addEventListener('click', () => {
+    if (isDark) {
+        
+        document.documentElement.style.setProperty('--background-color','#fefefe');
+        document.documentElement.style.setProperty('--text-color','#333333');
+        isDark = false;
+    } else {
+        document.documentElement.style.setProperty('--background-color','#37383b');
+        document.documentElement.style.setProperty('--text-color','#8f8a8a');
+        isDark = true;
+    }
+})
+
+
 
 
 // project-grid--js
@@ -67,3 +85,4 @@ fetch(
     }
   })
   .catch((e) => console.log(e));
+
